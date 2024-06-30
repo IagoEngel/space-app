ac8c481134b553bfbe04768f7ec28079b28183c2
Iago Engel Serafim <iagoengel@yahoo.com>
2024-06-30 15:52:50 -0300

    Aula 2(Componentes iniciais e fontes)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import styled from "styled-components"
 import EstilosGlobais from "./componentes/EstilosGlobais"
+import Cabecalho from "./componentes/Cabecalho"
+import BarraLateral from "./componentes/BarraLateral"
+import Banner from "./componentes/Banner"
 
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -12,6 +15,9 @@ function App() {
   return (
     <FundoGradiente>
       <EstilosGlobais />
+      <Cabecalho />
+      <BarraLateral />
+      <Banner />
     </FundoGradiente>
   )
 }
